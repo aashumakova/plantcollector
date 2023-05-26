8ad3aa7af3b6fbf57ad6bf6fe333a797433a8015
aashumakova <aashumakova@gmail.com>
2023-05-26 14:05:06 -0700

    about and all plants renders

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+plants = [
+    {'name': 'Morgan', 'kingdom': 'Plantae', 'species': 'Monstera xanthospatha', 'performance': 'bright to medium-bright indirect light','age': 1},
+    {'name': 'Audrey', 'kingdom': 'Plantae', 'species': 'Ficus benghalensis', 'performance': 'bright indirect light','age': 1},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def plants_index(request):
+    return render(request, 'plants/index.html', { 'plants': plants
+    })
